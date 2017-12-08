a84e93d5f374e10b7956a2827bc601544a73c3b0
Tito Valiente <titolvaliente@gmail.com>
2017-12-08 15:34:07 -0600

    finished adding albums table

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,9 +4,10 @@
 
 USE codeup_test_db;
 
+
 DROP TABLE IF EXISTS albums;
 
-CREATE TABLE quotes (
+CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(50) DEFAULT 'NONE',
     name  VARCHAR(100) NOT NULL,
