383f7b7d1a27881307ad5b2bd47d753275f8251c
Tito Valiente <titolvaliente@gmail.com>
2017-12-08 12:02:33 -0600

    starting table exercise

---
+++ albums_migration.sql
@@ -0,0 +1,17 @@
+-- noinspection SqlNoDataSourceInspectionForFile
+
+-- noinspection SqlDialectInspectionForFile
+
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE quotes (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(50) DEFAULT 'NONE',
+    name  VARCHAR(100) NOT NULL,
+    release_date INT NOT NULL,
+    sales FLOAT NOT NULL,
+    genre VARCHAR (50),
+    PRIMARY KEY (id)
+);
