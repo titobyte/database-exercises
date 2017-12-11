51bd39acd3412962bf5d3c25a300f96973bb57c2
Tito Valiente <titolvaliente@gmail.com>
2017-12-11 14:50:07 -0600

    added comments

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,17 +1,17 @@
 USE codeup_test_db;
 
-SELECT *
-FROM albums
-WHERE release_date > 1991;
-
-SELECT *
-FROM albums
-WHERE genre = 'disco';
-
-SELECT *
-FROM albums
-WHERE artist = 'Whitney Houston';
-
+-- SELECT *
+-- FROM albums
+-- WHERE release_date > 1991;
+--
+-- SELECT *
+-- FROM albums
+-- WHERE genre = 'disco';
+--
+-- SELECT *
+-- FROM albums
+-- WHERE artist = 'Whitney Houston';
+--
 
 
 
